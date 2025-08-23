573bfb8632e99e9afd89ec80d40b1a263fdda669
khrsany <alabass.alkhrsany.02@gmail.com>
2025-08-23 13:41:08 +0300

    tehtävät suoritettu

---
+++ moduuli 1.1/src/tehtava_2.java
@@ -0,0 +1,8 @@
+public class tehtava_2 {
+    public static void main(String[] args) {
+    System.out.println("    *\n" +
+                        "   ***\n" +
+                          "  *****\n" +
+                            " *******");
+    }
+}
